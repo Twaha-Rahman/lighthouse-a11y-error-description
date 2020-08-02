0fbfada1902c62ab8dac6c25aa227c720e0e78a2
Twaha-Rahman <mahi6703890@gmail.com>
2020-08-02 22:56:54 +0600

    Update Lighthouse config

---
+++ lighthouserc.js
@@ -0,0 +1,16 @@
+/**
+ * @file - Conifiguration for LightHouse CI. Vist the link below
+ * for more details ->
+ * https://github.com/GoogleChrome/lighthouse/blob/master/docs/configuration.md
+ */
+
+module.exports = {
+  ci: {
+    collect: {
+      url: ['http://localhost:8080/'],
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
